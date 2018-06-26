8b179cc51d608a41804e2f826d525e1274c430b0
Taeer Bar-Yam <taeer@necsi.edu>
2018-06-26 13:15:05 -0400

    alert ok button

--- app/src/main/java/me/bar_yam/taeer/paclist/MainActivity.java
+++ app/src/main/java/me/bar_yam/taeer/paclist/MainActivity.java
@@ -3,6 +3,7 @@ package me.bar_yam.taeer.paclist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final AlertDialog.Builder loginResponse = new AlertDialog.Builder(this);
+        final AlertDialog.Builder loginResponse = new AlertDialog.Builder(this)
+          .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id){
+              dialog.cancel();
+            }
+          });
+
         final Button loginButton = (Button) findViewById(R.id.login_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
           public void onClick(View v) {
