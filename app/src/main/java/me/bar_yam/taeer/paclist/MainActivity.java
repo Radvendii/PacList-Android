3cc4e4f929ce9203aab7cc1517711d1734276668
Taeer Bar-Yam <taeer@necsi.edu>
2018-06-26 12:31:16 -0400

    bugfix

--- app/src/main/java/me/bar_yam/taeer/paclist/MainActivity.java
+++ app/src/main/java/me/bar_yam/taeer/paclist/MainActivity.java
@@ -1,9 +1,11 @@
 package me.bar_yam.taeer.paclist;
 
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.app.AlertDialog;
 import android.view.View;
-import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
         final AlertDialog.Builder adb = new AlertDialog.Builder(this);
         adb.setTitle("foo");
-        final Button loginButton = findViewById(R.id.login_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
           public void onClick(View v) {
-            EditText name = findViewById(R.id.name_edit);
-            EditText username = findViewById(R.id.username_edit);
-            EditText password = findViewById(R.id.password_edit);
+            EditText name     = (EditText) findViewById(R.id.name_edit);
+            EditText username = (EditText) findViewById(R.id.username_edit);
+            EditText password = (EditText) findViewById(R.id.password_edit);
             AlertDialog ad = adb.create();
             ad.show();
           }
