6b56b0aeeddb9274e6187c1ce60b0f1b9957378b
Taeer Bar-Yam <taeer@necsi.edu>
2018-06-27 11:01:36 -0400

    second view for the list

---
+++ app/src/main/java/me/bar_yam/taeer/paclist/ListActivity.java
@@ -0,0 +1,18 @@
+package me.bar_yam.taeer.paclist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_list);
+  }
+}
--- app/src/main/java/me/bar_yam/taeer/paclist/MainActivity.java
+++ app/src/main/java/me/bar_yam/taeer/paclist/MainActivity.java
@@ -7,41 +7,42 @@ import android.content.DialogInterface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        final AlertDialog.Builder loginResponse = new AlertDialog.Builder(this)
-          .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id){
-              dialog.cancel();
-            }
-          });
+    final AlertDialog.Builder loginResponse = new AlertDialog.Builder(this)
+      .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int id){
+          dialog.cancel();
+        }
+      });
 
-        final Button loginButton = (Button) findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-          public void onClick(View v) {
-            String name     = ((EditText) findViewById(R.id.name_edit)).getText().toString();
-            String username = ((EditText) findViewById(R.id.username_edit)).getText().toString();
-            String password = ((EditText) findViewById(R.id.password_edit)).getText().toString();
-            if(username.equals("") || password.equals("")){
-              loginResponse.setTitle("Error");
-              loginResponse.setMessage("Please enter both a username and password.");
-            }
-            else if(username.equals("foo") && password.equals("bar")){
-              loginResponse.setTitle("Sucess");
-              loginResponse.setMessage("Username and Password are correct!");
-            }
-            else {
-              loginResponse.setTitle("Try Again");
-              loginResponse.setMessage("Incorrect information was entered. Please send an email to request the username and password.");
-            }
-            loginResponse.show();
-          }
-        });
-    }
+    final Button loginButton = (Button) findViewById(R.id.login_button);
+    loginButton.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        String name     = ((EditText) findViewById(R.id.name_edit)).getText().toString();
+        String username = ((EditText) findViewById(R.id.username_edit)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password_edit)).getText().toString();
+        if(username.equals("") || password.equals("")){
+          loginResponse.setTitle("Error");
+          loginResponse.setMessage("Please enter both a username and password.");
+          loginResponse.show();
+        }
+        else if(username.equals("foo") && password.equals("bar")){
+          startActivity(new Intent(getApplicationContext(), ListActivity.class));
+        }
+        else {
+          loginResponse.setTitle("Try Again");
+          loginResponse.setMessage("Incorrect information was entered. Please send an email to request the username and password.");
+          loginResponse.show();
+        }
+      }
+    });
+  }
 }
